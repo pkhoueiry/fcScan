cb5081d1484e7fc22e9eecea6c6e0d39af2abbdd
ghk9 <ghiwa.khalil@lau.edu>
2018-08-20 16:24:19 +0300

    updated

--- R/mapCluster.R
+++ R/mapCluster.R
@@ -14,7 +14,7 @@ mapCluster <- function(x, kent = NULL, chrom.sizes , chain) {
     ##assuming we have a GRange object as the final result
 
     input.bed <- data.frame(chrom = x[,1], start = x[,2],
-            end = x[,3], name = x[,4], score = x[,5], strand = x[,6] )
+                            end = x[,3], name = x[,4], score = x[,5], strand = x[,6] )
     tmp_dir = tempdir()
     write.table(input.bed,paste0(tmp_dir,"/input.bed"),
                 sep = "\t",quote = FALSE, row.names = FALSE,col.names  = FALSE)
@@ -25,28 +25,25 @@ mapCluster <- function(x, kent = NULL, chrom.sizes , chain) {
 
     if(is.null(kent)){
         system(sprintf("bedToPsl %s %s %s",chrom.sizes ,
-            paste0(tmp_dir,"/input.bed"),paste0(tmp_dir,"/input.psl")))
+                       paste0(tmp_dir,"/input.bed"),paste0(tmp_dir,"/input.psl")))
 
         system(sprintf("pslMap -chainMapFile -swapMap -mapInfo=%s %s %s %s",
-    paste0(tmp_dir,"/results.txt"), paste0(tmp_dir,"/input.psl"),
-    chain ,paste0(tmp_dir,"/output.psl")))
+                       paste0(tmp_dir,"/results.txt"), paste0(tmp_dir,"/input.psl"),
+                       chain ,paste0(tmp_dir,"/output.psl")))
     } else{
         system(sprintf("%s/bedToPsl %s %s %s",kent,chrom.sizes,
-            paste0(tmp_dir,"/input.bed"),paste0(tmp_dir,"/input.psl")))
+                       paste0(tmp_dir,"/input.bed"),paste0(tmp_dir,"/input.psl")))
         system(sprintf("%s/pslMap -chainMapFile -swapMap -mapInfo=%s %s %s %s",
-    kent, paste0(tmp_dir,"/results.txt"), paste0(tmp_dir,"/input.psl"),chain
-    ,paste0(tmp_dir,"/output.psl")))
+                       kent, paste0(tmp_dir,"/results.txt"), paste0(tmp_dir,"/input.psl"),chain
+                       ,paste0(tmp_dir,"/output.psl")))
     }
 
-     results = read.delim(paste0(tmp_dir,"/results.txt"),
-                          stringsAsFactors = FALSE ,fill = TRUE)
+    results = read.delim(paste0(tmp_dir,"/results.txt"),
+                         stringsAsFactors = FALSE ,fill = TRUE)
     ##mappedTName mappedTStart mappedTEnd mappedStrand mappedAligned
     ##srcTName srcTStart srcTEnd srcStrand
-     results = results[,c(21,22,23,24,5,6,7,8)]
-     colnames(results) = c("targetChr","targetStart","targetEnd","targetStrand",
-     "srcChr","srcStart","srcEnd","srcStrand")
-     #results[is.na(results)] <- "--"
-     return(results)
+    results = results[,c(21,22,23,24,5,6,7,8)]
+    return(results)
 }
 
 
