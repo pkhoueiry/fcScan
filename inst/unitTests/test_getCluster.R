2f04987775f7db27156d0787f8accbe351a36484
abedkurdi <abed_kurdi@hotmail.com>
2019-07-24 11:17:23 +0300

    Added one more example - GRanges

--- inst/unitTests/test_getCluster.R
+++ inst/unitTests/test_getCluster.R
@@ -34,12 +34,12 @@ t2 = GRanges(
 )
 
 t3 = GRanges(
-    seqnames = Rle("chr1", 2),
-    ranges = IRanges(c(11L, 26L), end = c(20L, 35L)),
-    strand = Rle("+", 2),
-    sites = as.character(c("s1,s2", "s2,s1")),
-    isCluster = as.logical(Rle(TRUE, 2)),
-    status = as.character(Rle(c("PASS"), 2))
+    seqnames = Rle("chr1", 3),
+    ranges = IRanges(c(11L, 26L, 95L), end = c(20L, 35L, 105L)),
+    strand = Rle("+", 3),
+    sites = as.character(c("s1,s2", "s2,s1", "s2,s1")),
+    isCluster = as.logical(Rle(TRUE, 3)),
+    status = as.character(Rle(c("PASS"), 3))
 )
 
 t4 = GRanges(
@@ -72,11 +72,11 @@ t7 = GRanges(
 
 t8 = GRanges(
     seqnames = Rle("chr1", 4),
-    ranges = IRanges(c(11L, 18L, 26L, 88L), end = c(55L, 55L, 75L, 135L)),
+    ranges = IRanges(c(11L, 18L, 71L, 88L), end = c(55L, 68L, 120L, 135L)),
     strand = Rle("+",4),
-    sites = as.character(c("s1,s2,s2,s1,s2,s1,s1", "s2,s2,s1,s2,s1,s1", "s2,s1,s2,s1,s1,s2,s1", "s2,s2,s1,s2,s1,s1,s2")),
-    isCluster = as.logical(c(FALSE, FALSE, TRUE, TRUE)),
-    status = as.character(c("orderFail", "orderFail", "PASS", "PASS"))
+    sites = as.character(c("s1,s2,s2,s1,s2,s1,s1", "s2,s2,s1,s2,s1,s1,s2", "s1,s2,s2,s1,s2,s1", "s2,s2,s1,s2,s1,s1,s2")),
+    isCluster = as.logical(c(FALSE, TRUE, FALSE, TRUE)),
+    status = as.character(c("orderFail", "PASS", "orderFail", "PASS"))
 )
 
 t9 = GRanges(
@@ -98,18 +98,18 @@ t10 = GRanges(
 )
 
 t11 = GRanges(
-    seqnames = Rle("chr1", 1),
-    ranges = IRanges(c(11L), end = c(20L)),
-    strand = Rle("*", 1),
-    sites = as.character(c("s1,s2")),
-    isCluster = as.logical(Rle(TRUE, 1)),
-    status = as.character(Rle("PASS", 1))
+    seqnames = Rle("chr1", 3),
+    ranges = IRanges(c(11L, 26L, 95L), end = c(20L, 35L, 105L)),
+    strand = Rle("*", 3),
+    sites = as.character(c("s1,s2", "s2,s1", "s2,s1")),
+    isCluster = as.logical(Rle(TRUE, 3)),
+    status = as.character(Rle("PASS", 3))
 )
 
 x3 = data.frame(seqnames = rep("chr1", times = 6),
                start = c(10,17,25,40,42,55),
                end = c(15,20,30,50,56,70),
-               strand = c("+", "-", "-", "-", "+", "+"),
+               strand = c("+", "+", "+", "+", "+", "+"),
                site = c("s1","s2","s2","s2","s1","s1"))
 
 t12 = GRanges(
@@ -184,6 +184,23 @@ t17 = GRanges(
     status = as.character(c("ExcludedSites", "ExcludedSites", "PASS"))
 )
 
+x7 = GRanges(
+    seqnames = Rle("chr1", 16),
+    ranges = IRanges(start = c(10L,17L,25L,27L,32L,41L,47L,60L,70L,87L,94L,99L,107L,113L,121L,132L),
+    end = c(15L,20L,30L,35L,40L,48L,55L,68L,75L,93L,100L,105L,113L,120L,130L,135L)),
+    strand = Rle("+",16),
+    site = c("s1","s2","s2","s1","s2","s1","s1","s2",
+        "s1","s2","s2","s1","s2","s1","s1","s2"))
+
+t18 = GRanges(
+    seqnames = Rle("chr1", 2),
+    ranges = IRanges(start = c(10L, 87L), end = c(30L, 105L)),
+    strand = Rle("*",2),
+    sites = as.character(c("s1,s2,s2", "s2,s2,s1")),
+    isCluster = as.logical(c(TRUE, TRUE)),
+    status = as.character("PASS","PASS")
+)
+
 test_getCluster <- function() {
     #general test t1#
     checkEquals(getCluster(x1, w = 11, c = c("s1" = 1, "s2" = 1), greedy = FALSE, order = c("s1","s2"), s = "+", verbose = TRUE), t1)
@@ -192,7 +209,7 @@ test_getCluster <- function() {
     #test t2 - get ONE cluster only#
     checkEquals(getCluster(x2, w = 20, c = c("s1" = 1,"s2" = 2), greedy = TRUE, overlap = 0, s = "+", order = c("s1","s2","s2"), verbose = TRUE), t2)
     #test t3 - get TWO clusters only#
-    #-->checkEquals(getCluster(x1, w = 10, c = c("s1" = 1, "s2" = 1), greedy = FALSE, s = "+", verbose = TRUE), t3)
+    checkEquals(getCluster(x1, w = 10, c = c("s1" = 1, "s2" = 1), greedy = FALSE, s = "+", verbose = TRUE), t3)
     #test t4 - get one cluster having only sites s1 and s3#
     checkEquals(getCluster(x1, w = 15, c = c("s1" = 1, "s3" = 1), greedy = TRUE, s = "+", verbose = TRUE), t4)
     #test t5 - get NULL#
@@ -202,17 +219,17 @@ test_getCluster <- function() {
     #test t7 - get two clusters and one orderFail#
     checkEquals(getCluster(x1, w = 50, c = c("s1" = 2, "s2" = 3), greedy = FALSE, order = c("s1","s2","s2","s1","s2"), s = "+", verbose = TRUE), t7)
     #test t8 - get two clusters and two orderFail#
-    #-->checkEquals(getCluster(x1, w = 50, c = c("s1" = 2, "s2" = 2), greedy = TRUE, order = c("s2","s1","s1","s2"), s = "+", verbose = TRUE), t8)
+    checkEquals(getCluster(x1, w = 50, c = c("s1" = 2, "s2" = 2), greedy = TRUE, order = c("s2","s1","s1","s2"), s = "+", verbose = TRUE), t8)
     #test t9 - get clusters not overlapping#
     checkEquals(getCluster(x1, w = 16, c = c("s1" = 1, "s2" = 1), greedy = FALSE, verbose = TRUE), t9)
     #test t10 - get overlapping clusters#
     checkEquals(getCluster(x1, w = 16, c = c("s1" = 1, "s2" = 1), greedy = FALSE, overlap = -3, verbose = TRUE), t10)
     #test t11 - get clusters with minimum gap of 6#
-    #-->checkEquals(getCluster(x1, w = 10, c = c("s1" = 1, "s2" = 1), greedy = FALSE, overlap = 6, verbose = TRUE), t11)
+    checkEquals(getCluster(x1, w = 10, c = c("s1" = 1, "s2" = 1), greedy = FALSE, overlap = 6, verbose = TRUE), t11)
     #test t12 - get two clusters regardless of strand information#
-    #-->checkEquals(getCluster(x3, w = 16, c = c("s1" = 1, "s2" = 1), greedy = TRUE, overlap = -2, verbose = TRUE), t12)
+    checkEquals(getCluster(x3, w = 16, c = c("s1" = 1, "s2" = 1), greedy = TRUE, overlap = -2, verbose = TRUE), t12)
     #test t13 - regardless of strand information#
-    #-->checkEquals(getCluster(x3, w = 16, c = c("s1" = 1, "s2" = 1), greedy = TRUE, verbose = TRUE), t13)
+    checkEquals(getCluster(x3, w = 16, c = c("s1" = 1, "s2" = 1), greedy = TRUE, verbose = TRUE), t13)
     #test t14 - get clusters of s1 and s3 sites with s2 excluded#
     checkEquals(getCluster(x4, w = 20, c = c("s1" = 1, "s2" = 0, "s3" = 1), greedy = TRUE, verbose = TRUE), t14)
     #test t15 - get one TRUE cluster and one with excluded sites#
@@ -223,4 +240,6 @@ test_getCluster <- function() {
     checkEquals(getCluster(x5, w = 40, c = c("s1" = 1, "s2" = 2, "s3" = 0), greedy = FALSE, verbose = TRUE), NULL)
     #test t18 - get three consecutive ExcludedSites and One TRUE cluster#
     checkEquals(getCluster(x6, w = 40, c = c("s1" = 1, "s2" = 2, "s3" = 0), order = c("s1", "s2", "s2"), greedy = TRUE, verbose = TRUE), t17)
+    #test t19 - input GRanges#
+    checkEquals(getCluster(x7, w = 25, c = c("s1"=1,"s2"=2)), t18)
 }
